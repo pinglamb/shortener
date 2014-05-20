61b0d4a92f305dc837d174e648a5978988980b5f
pinglamb <pinglambs@gmail.com>
2014-05-20 10:51:19 +0800

    Added hyphen to available key chars

--- lib/shortener.rb
+++ lib/shortener.rb
@@ -6,16 +6,16 @@ module Shortener
   autoload :ShortenUrlInterceptor, "shortener/shorten_url_interceptor"
 
   CHARSETS = {
-    :alphanum => ('a'..'z').to_a + (0..9).to_a,
-    :alphanumcase => ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a }
+    :alphanum => ('a'..'z').to_a + (0..9).to_a + ['-'],
+    :alphanumcase => ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a + ['-'] }
 
   # default key length: 5 characters
   mattr_accessor :unique_key_length
   self.unique_key_length = 5
 
   # character set to chose from:
-  #  :alphanum     - a-z0-9     -  has about 60 million possible combos
-  #  :alphanumcase - a-zA-Z0-9  -  has about 900 million possible combos
+  #  :alphanum     - a-z0-9\-     -  has about 60 million possible combos
+  #  :alphanumcase - a-zA-Z0-9\-  -  has about 900 million possible combos
   mattr_accessor :charset
   self.charset = :alphanum
 
